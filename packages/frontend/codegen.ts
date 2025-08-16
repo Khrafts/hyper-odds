9fd391f9960f92dc4ad32d27dce81ec17bc871a1
Uyoyou Saint Uwuseba <uwusebauyoyou@gmail.com>
2025-08-16 09:17:52 +0100

    feat: install and configure GraphQL dependencies
    - Install Apollo Client and GraphQL core library
    - Add GraphQL codegen tools and TypeScript plugins
    - Create codegen configuration with client preset
    - Add codegen scripts to package.json
    - Configure proper scalar types and naming conventions

---
+++ packages/frontend/codegen.ts
@@ -0,0 +1,57 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+import { env } from './src/lib/env'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:8000/graphql',
+  documents: ['src/**/*.{ts,tsx}', '!src/graphql/__generated__/**/*'],
+  generates: {
+    'src/graphql/__generated__/': {
+      preset: 'client',
+      plugins: [],
+      presetConfig: {
+        gqlTagName: 'gql',
+        fragmentMasking: false,
+      },
+      config: {
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+        withMutationFn: true,
+        withResultType: true,
+        withMutationOptionsType: true,
+        withRefetchFn: true,
+        pureMagicComment: true,
+        omitOperationSuffix: true,
+        skipTypename: false,
+        enumsAsTypes: false,
+        dedupeOperationSuffix: true,
+        namingConvention: {
+          typeNames: 'PascalCase',
+          enumValues: 'UPPER_CASE',
+        },
+        scalars: {
+          DateTime: 'string',
+          Date: 'string',
+          BigInt: 'string',
+          Decimal: 'string',
+          JSON: 'Record<string, any>',
+        },
+        avoidOptionals: {
+          field: true,
+          inputValue: false,
+          object: false,
+          defaultValue: false,
+        },
+      },
+    },
+    './src/graphql/__generated__/graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+  hooks: {
+    afterAllFileWrite: ['prettier --write'],
+  },
+}
+
+export default config
